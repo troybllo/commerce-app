a8d28e358ffe5182172bda002b25f9fb06fc5035
troybello <troybello25@gmail.com>
2024-03-14 17:34:55 -0400

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import ButtonIcon from "./ui/button-icon";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col p-6 gap-5">
+      <div className="flex  bg-white rounded w-full p-10 items-start h-30">
+        <p className="text-black font-extrabold text-[32px]">ACOM</p>
+      </div>
+      <div className="mt-40">
+        <div className="flex flex-row">
+          <p className="text-[90px]">
+            <strong>Welcome to ACOM</strong>
+          </p>
+        </div>
+
+        <div className=" flex mt-3 flex-col w-1/6 gap-6">
+          <Link
+            className="flex rounded-lg border-white border-2  p-4 "
+            href="_"
+          >
+            <p>Catalog</p>
+            <ButtonIcon />
+          </Link>
+          <Link
+            className="flex flex-row rounded-lg border-white border-2 p-4"
+            href="_"
+          >
+            <p>About Us</p>
+            <ButtonIcon />
+          </Link>
+        </div>
+        <div className="flex items-start justify-end">
+          <Image
+            src="/hoodie.png"
+            width={500}
+            height={400}
+            className=""
+            alt="blah"
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/ui/button-icon.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function ButtonIcon() {
+  return (
+    <div>
+      <Image src="/arrow.svg" width={20} height={20} className="_" alt="_" />
+    </div>
+  );
+}
