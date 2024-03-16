0efb8e25f8f947a5b368d7cc6a5a198d206d1510
troybello <troybello25@gmail.com>
2024-03-16 06:25:39 -0400

    Add product data

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
-    <div>
-      <div>.</div>
+    <div className="flex flex-row flex-wrap p-9 w-full gap-20 ">
+      <div className="w-1/4">1</div>
+      <div className="w-1/4">2</div>
+      <div className="w-1/4">3</div>
+      <div className="w-1/4">4</div>
+      <div className="w-1/4">5</div>
+      <div className="w-1/4">6</div>
     </div>
   );
 }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,12 +3,14 @@ import Sidenav from "../ui/dashboard/sidenav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div>
         <NavBar />
-        <Sidenav />
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex flex-row p-6 md:overflow-y-auto md:p-12 gap-8">
+        <Sidenav />
+        {children}
+      </div>
     </div>
   );
 }
