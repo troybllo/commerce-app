4069842945347ba268a709d8ad49c9f5b4bf0e10
troybello <troybello25@gmail.com>
2024-03-22 23:36:02 -0400

    fix

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,35 +1,30 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { products } from "@/app/lib/placeholder-data";
-import { useRouter } from "next/router";
 
 const links = [
-  { name: "All", category: "/dashboard" },
-  { name: "Tops", category: "/Tops" },
-  { name: "Bottoms", category: "/dashboard/Product/Bottoms" },
-  { name: "Outerwear", category: "/dashboard/Product/OuterOuterwear" },
-  { name: "Accessories", category: "/dashboard/Product/Accessories" },
-  { name: "Footwear", category: "/dashboard/Product/FootFootwear" },
-  { name: "Pets", category: "/dashboard/Product/Pets" },
+  { name: "All", href: "/dashboard" },
+  { name: "Tops", href: "/Tops" },
+  { name: "Bottoms", href: "/dashboard/Product/Bottoms" },
+  { name: "Outerwear", href: "/dashboard/Product/Outerwear" },
+  { name: "Accessories", href: "dashboard/Product/Accessories" },
+  { name: "Footwear", href: "dashboard/Product/Footwear" },
+  { name: "Pets", href: "dashboard/Product/Pets" },
 ];
 
 export default function Links() {
-  const router = useRouter();
   const [productData, setProductData] = useState(products);
-  const { category } = router.query;
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  useEffect(() => {
-    if (category) {
-      const filteredProducts = products.filter(
-        (product) => product.category === category,
-      );
-      setProductData(filteredProducts);
-    } else {
-      setProductData(products);
-    }
-  }, [category]);
+  const handleClick = (category: any) => {
+    setSelectedCategory(category);
+    const filteredProducts = products.filter(
+      (product) => product.category === category,
+    );
+    setProductData(filteredProducts);
+  };
 
   const [isHover, setIsHover] = useState(null);
 
@@ -46,16 +41,11 @@ export default function Links() {
       {links.map((link) => {
         return (
           <li key={link.name}>
-            <Link
-              href={{
-                pathname: "/Products",
-                query: { category: link.category },
-              }}
-              className="text-gray-400"
-            >
+            <Link href={link.href} className="text-gray-400">
               <p
                 onMouseEnter={() => onHover(link.name)}
                 onMouseOut={onExit}
+                onClick={() => handleClick(link.name)}
                 style={{ color: isHover === link.name ? "white" : "gray" }}
               >
                 {link.name}
