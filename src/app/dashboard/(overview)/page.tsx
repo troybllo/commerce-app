6544698ae9611572d0080fcb15f5ef9131636e85
troybello <troybello25@gmail.com>
2024-04-01 03:21:39 -0400

    fix

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -1,6 +1,7 @@
 import { fetchFilteredProducts, fetchProducts } from "@/app/lib/data";
 import ProductCard from "@/app/ui/dashboard/product-card";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Page({
   searchParams,
@@ -13,17 +14,19 @@ export default async function Page({
   const data = await fetchFilteredProducts(query);
 
   return (
-    <div className="flex flex-row flex-wrap p-9 w-full gap-20 ">
-      {data.map((product) => (
-        <div
-          key={product.id}
-          className="w-1/4 border-white border-2 rounded-lg p-6"
-        >
-          <Link href={`/dashboard/Product/${product.id}`}>
-            <ProductCard product={product} />
-          </Link>
-        </div>
-      ))}
-    </div>
+    <Suspense>
+      <div className="flex flex-row flex-wrap p-9 w-full gap-20 ">
+        {data.map((product) => (
+          <div
+            key={product.id}
+            className="w-1/4 border-white border-2 rounded-lg p-6"
+          >
+            <Link href={`/dashboard/Product/${product.id}`}>
+              <ProductCard product={product} />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Suspense>
   );
 }
